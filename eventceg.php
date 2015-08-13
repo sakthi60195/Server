fed5ed76973acc0ab22a393b2f1cf987a470a67b
Sakthivel <sakthipvmj@gmail.com>
2015-08-12 20:42:36 -0700

    first commit

---
+++ eventceg.php
@@ -0,0 +1,9 @@
+<?php
+
+$data0=array("name"=>"Management Studies","date"=>"1439110170989","club"=>"CSAU","description"=>"An event that is used for");
+$data1=array("name"=>"Studies","date"=>"1439110152319","club"=>"Leo","description"=>"An event that is used for");
+$data= array();
+$data[0]=$data0;
+$data[1]=$data1;
+echo json_encode($data);
+?>
